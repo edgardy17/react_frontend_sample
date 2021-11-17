b98e8100d8b51435ba2e7d84537ff44b93d8e5f9
Edgardy Reyes <ear53@duke.edu>
2021-11-17 01:02:37 -0500

    Revert to older version

--- src/AuthorPage.js
+++ src/AuthorPage.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import {Container, Row} from "shards-react";
 
-//change so that it redirects to /productX.html instead of just drop down menu
-
 const AuthorPage = (props) => {
     var posts = props.posts;
     const postList = posts.map((post)=>{
